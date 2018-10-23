abceeec9c8670b4c4883c943f727b7dd4aa15b6e
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-10-23 20:58:25 +0600

    Project Opened

---
+++ app/src/main/java/com/fahimshahrierrasel/gasmonitor/modeldata/CoordinatesData.java
@@ -0,0 +1,35 @@
+package com.fahimshahrierrasel.gasmonitor.modeldata;
+
+import com.fahimshahrierrasel.gasmonitor.model.Coordinate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CoordinatesData {
+    List<Coordinate> data;
+
+    public CoordinatesData() {
+        setData();
+    }
+
+    public List<Coordinate> getData() {
+        return data;
+    }
+
+    public void setData(List<Coordinate> data) {
+        this.data = data;
+    }
+
+    private void setData() {
+        data = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            Coordinate coordinate = new Coordinate();
+            coordinate.setX(i);
+            coordinate.setY(random.nextInt(10000));
+            data.add(coordinate);
+        }
+    }
+}
